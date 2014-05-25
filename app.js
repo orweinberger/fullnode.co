a105aa58e9a9e05c752d3dfcb100f8c0b94ff46d
orweinberger <orweinberger@gmail.com>
2014-05-25 22:23:08 +0300

    Added express to start working on the frontend

--- app.js
+++ app.js
@@ -3,46 +3,57 @@ var joola = require('./lib/joola')
 var common = require('./lib/common');
 var blockchain = require('./lib/blockchain');
 var linode = require('./lib/providers/linode');
-var logger = require('winston');
+var winston = require('winston');
 var moment = require('moment');
-var async = require('async');
+
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
+var app = express();
+
 
 var total_in_usd;
 var coins_to_move;
 
 function queue() {
   blockchain.checkBalance(config.general.hotWalletAddress, function (err, balance) {
-    logger.info("checking balance: " + balance);
+    winston.info("checking balance: " + balance);
     if (!err) {
       common.getPrice(function (err, price) {
-        logger.info('getting price ' + price)
+        winston.info('getting price ' + price)
         if (!err) {
           total_in_usd = Math.floor(parseFloat(price) * parseFloat((balance - 10000) / 100000000));
           coins_to_move = (config.providers.linode.price / parseFloat(price)) * 100000000;
           if (total_in_usd >= config.providers.linode.price) {
             common.queueCoins(coins_to_move, config.general.coldWalletAddress, function (err) {
-              logger.info("Coins queued " + coins_to_move);
+              winston.info("Coins queued " + coins_to_move);
               if (!err) {
                 common.queueServer('linode', function (err) {
                   if (!err) {
-                    logger.info("Server queued " + coins_to_move);
+                    winston.info("Server queued " + coins_to_move);
                   }
                   else {
-                    logger.error("[queueServer] " + err);
+                    winston.error("[queueServer] " + err);
                   }
                 });
               }
               else
-                logger.error("[queueCoins] " + err);
+                winston.error("[queueCoins] " + err);
             });
           }
         }
         else
-          logger.error("[getPrice] Error! " + err);
+          winston.error("[getPrice] Error! " + err);
       })
     }
     else
-      logger.error("[checkBalance] Error! " + err);
+      winston.error("[checkBalance] Error! " + err);
   });
 }
 
@@ -51,35 +62,35 @@ function run() {
     if (coin) {
       blockchain.moveCoins(coin, function (err, res) {
         if (err)
-          logger.error("[moveCoins] " + err);
+          winston.error("[moveCoins] " + err);
         else {
-          logger.info("Moved coins");
+          winston.info("Moved coins");
           common.dequeueCoins(coin, function (err, res) {
             if (err)
-              logger.error("[dequeueCoins] " + err);
+              winston.error("[dequeueCoins] " + err);
             else {
-              logger.info("dequeued coins");
+              winston.info("dequeued coins");
               common.getServerQueue(function (err, server) {
                 if (err)
-                  logger.error("[getServerQueue] " + err);
+                  winston.error("[getServerQueue] " + err);
                 else {
-                  logger.info("Got server queue");
+                  winston.info("Got server queue");
                   linode.provisionServer(server, function (err, srv) {
                     if (err)
-                      logger.error("[provisionServer] " + err);
+                      winston.error("[provisionServer] " + err);
                     else {
-                      logger.info("Provisioned server");
+                      winston.info("Provisioned server");
                       common.dequeueServer(server, function (err, res) {
                         if (err)
-                          logger.error("[dequeueServer] " + err);
+                          winston.error("[dequeueServer] " + err);
                         else {
-                          logger.info("dequeued Server");
+                          winston.info("dequeued Server");
                           joola.beacon(config.joola.collection, {"timestamp": null, "servers": 1, "ipaddress": srv.ip, "dc": srv.dc, "coins_moved": coin.amount, "serverid": srv.serverid}, function (err, doc) {
                             if (err)
-                              logger.error("[beacon] " + err);
+                              winston.error("[beacon] " + err);
                             else {
-                              logger.info("Pushed data into joola");
-                              logger.info(doc);
+                              winston.info("Pushed data into joola");
+                              winston.info(doc);
                             }
                           });
                         }
@@ -99,9 +110,9 @@ function run() {
 function runDel() {
   var deldate = new Date(moment().subtract('days', 30).format());
   common.checkDelete(deldate, function (err) {
-    logger.info("Checking if there are any servers that needs to be deleted")
+    winston.info("Checking if there are any servers that needs to be deleted")
     if (err)
-      logger.error("[checkDelete] " + err);
+      winston.error("[checkDelete] " + err);
   });
 }
 
@@ -115,3 +126,54 @@ setInterval(function () {
   runDel();
 }, config.general.runInterval);
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/callback', routes.callbackurl);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
+
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
---
+++ routes/index.js
@@ -0,0 +1,4 @@
+exports.callbackurl = function (req, res) {
+  console.log(req.body);
+  res.send(200);
+}
