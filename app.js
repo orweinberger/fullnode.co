d56880956117a9d8d34deebda69ac3944e0ce007
orweinberger <orweinberger@gmail.com>
2014-05-26 01:19:08 +0300

    Added callback secret, dns names and some modifications to server provisioning function

--- app.js
+++ app.js
@@ -12,17 +12,15 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-
 var app = express();
-
+var debug = require('debug')('generated-express-app');
 
 function run() {
   common.getServerQueue(function (err, server) {
     if (err)
       winston.error("[getServerQueue] " + err);
-    else {
+    else if (server) {
       winston.info("Got server queue");
       linode.provisionServer(server, function (err, srv) {
         if (err)
@@ -30,6 +28,7 @@ function run() {
         else {
           winston.info("Provisioned server");
           common.dequeueServer(server, function (err, res) {
+            console.log("ssss" + server);
             if (err)
               winston.error("[dequeueServer] " + err);
             else {
@@ -67,6 +66,7 @@ setInterval(function () {
   runDel();
 }, config.general.runInterval);
 
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -77,37 +77,13 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes.index);
-app.use('/callback', routes.callbackurl);
+app.use('/', routes);
 
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.set('port', process.env.PORT || 3000);
 
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
 });
 
 
 module.exports = app;
-
-
-var server = app.listen(3000, function () {
-  console.log('Listening on port %d', server.address().port);
-});
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 var config = require('config').dev;
-var joola = require('../lib/joola')
+var joola = require('../lib/joola');
 var common = require('../lib/common');
 var blockchain = require('../lib/blockchain');
 var linode = require('../lib/providers/linode');
 var winston = require('winston');
 var moment = require('moment');
+var express = require('express');
+var router = express.Router();
 
-
-function queue() {
-  common.queueServer('linode', function (err) {
+function queue(dns) {
+  common.queueServer('linode', dns, function (err) {
     if (!err) {
       winston.info("Server queued");
     }
@@ -17,15 +18,23 @@ function queue() {
     }
   });
 }
-exports.index = function (req, res) {
-  res.render('index');
-}
 
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Express' });
+});
 
-exports.callbackurl = function (req, res) {
-  var order = req.body.order;
-  if (order.status == "completed" && order.total_native.cents == config.providers.linode.price * 100) {
-    queue();
+router.post('/callback', function (req, res) {
+  if (req.query.secret == config.general.callbackSecret) {
+    var order = req.body.order;
+    var dns = req.body.order.custom;
+    dns = dns.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-')
+    if (order.status == "completed" && order.total_native.cents == config.providers.linode.price * 100) {
+      queue(dns);
+    }
+    return res.send(200);
   }
-  return res.send(200);
-}
+  else
+    return res.send(403);
+});
+
+module.exports = router;
