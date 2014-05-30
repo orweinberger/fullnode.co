96fcf34912ee4772ba6083a49b65b2d855d87140
orweinberger <orweinberger@gmail.com>
2014-05-30 17:17:00 +0300

    Removed unnecessary favicon support

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var moment = require('moment');
 
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -69,7 +69,7 @@ setInterval(function () {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
+//app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
